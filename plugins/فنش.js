81612a29310417aa32a0066604eee7b2b612960b
Maro-Rezl <maro.bot6@gmail.com>
2024-08-06 12:29:11 +0300

    Update فنش.js

--- plugins/فنش.js
+++ plugins/فنش.js
@@ -5,7 +5,7 @@ let a7a = url + text;
 await conn.reply(m.chat, a7a, m);
 }
     handler.help = ['تبنيد'];
-    handler.tags = ['K U R O S A K I'];
+    handler.tags = ['M A R O'];
     handler.command = /^(فنش)$/i
 
     export default handler;
